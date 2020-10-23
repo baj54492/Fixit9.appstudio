19b098b8b1cccefc6010f00b13b4e86047a4ffd6
Brennan <71230741+baj54492@users.noreply.github.com>
2020-10-23 15:08:06 -0500

    final code
    final code

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -2,5 +2,5 @@ rdbPerson.onclick=function(){
   lblResponse.value=`I would agree that you are a ${$("input[name=rdbPerson]:checked").prop("value")} person too!`
 }
 btnSubmit2.onclick=function(){
-  ChangeForm(ABFavExercises)
+  ChangeForm(favExercises)
 }
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,21 +1,30 @@
-var exercises = ["pullups", "pushups", "situps", "jogging", "plank", "curls"]
-var coreExercises = ["situps", "plank"]
+var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
+var coreExercises = ["situps","plank"]
 
-favoriteExercise.onshow = function() {
-  for (i = 0; i <= exercises.length - 1; i++)
-    selExercises.addItem(exercises[i])
-}
-for (i = 0; i < coreExercises.length; i++) {
+favExercises.onshow=function(){
+ for (i = 0; i<= exercises.length-1;i++){
+   selExercises.addItem(exercises[i])
+   }
+for (i = 0;i<=coreExercises.length-1;i++){
   selExercises.addItem(coreExercises[i])
+  }
 }
 
-selExercises.onfocusout = function() {
-  let message = "Your chose:"
-  for (i = 0; i <= selExercises.text.length - 1; i++)
-    message = message + " " + selExercises.text[i]
-  lblExercises.value = message
+selExercises.onfocusout=function(){
+
 }
 
-btnMobileNavigation.onclick = function() {
-  ChangeForm(favExercises)
+btnFavoriteExercise.onclick=function(){
+  if (selExercises.text[0]=="situps" && selExercises.text[1]=="plank"){
+  lblExercises.value = `You chose ${selExercises.text[0]} and ${selExercises.text[1]} - those are the two core exercises.`
+} else { 
+  lblExercises.value = "You did not pick the two core exercises"
+  }
 }
+
+
+btnMobileNavigation.onclick=function(){
+  ChangeForm(mobileNav)
+}
+
+
